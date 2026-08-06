bc2e1f4b37fdd3bf8b74cc812a453caedc936010
decodism <decodism@protonmail.com>
2026-08-06 15:35:16 +0200

    perf: avoid unnecessary UI work

--- MacThrottle/MacThrottleApp.swift
+++ MacThrottle/MacThrottleApp.swift
@@ -2,15 +2,24 @@ import SwiftUI
 
 @main
 struct MacThrottleApp: App {
+    @State private var isMenuVisible = false
     @State private var monitor = ThermalMonitor()
 
     var body: some Scene {
         MenuBarExtra {
-            MenuContentView(monitor: monitor)
+            ZStack {
+                Color.clear
+                    .frame(width: 0, height: 0)
+                    .onAppear { isMenuVisible = true }
+                    .onDisappear { isMenuVisible = false }
+                if isMenuVisible {
+                    MenuContentView(monitor: monitor)
+                }
+            }
         } label: {
             MenuBarIcon(
                 pressure: monitor.pressure,
-                temperature: monitor.temperature,
+                temperature: monitor.showTemperatureInMenuBar ? monitor.temperature : nil,
                 showTemperature: monitor.showTemperatureInMenuBar
             )
         }
